95e08ae8683bdb8133ea8e8b948ef0d81c4b8cfe
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-11-27 14:47:31 +0000

    Feat: Create view for Booking to show user

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+from .models import BookingModel
 
 # Create your views here.
 def index(request):
     return render(request, "index.html",{})
+
+class BookingView(LoginRequiredMixin,generic.ListView):
+    """List bookings to current user"""
+    model = BookingModel
+    template_name = 'booking_overview.html'
+
+    def get_queryset(self):
+        return (
+            BookingModel.objects.filter(customer=self.request.user)
+            .order_by('day')
+        )
