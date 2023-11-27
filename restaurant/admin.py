ec91dca7cf87282d9d4ea109118dd152ce9de634
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-11-27 14:45:33 +0000

    Feat: Register Admin classes

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import BookingModel
 
 
-# Register the Admin classes for Booking using the decorator
+# Register the Admin classes for BookingModel using the decorator
 @admin.register(BookingModel)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'guests', 'day', 'time')
-
-    fields = ('last_name', 'first_name', 'guests', 'day', 'time')
+    list_display = ('created_on', 'last_name', 'first_name', 'guests', 'day', 'time')
+    list_filter = ('day', 'last_name', 'created_on')
