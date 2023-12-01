c9b477d84fe2fa39ed0523680b87cd43c6b410e7
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-12-01 15:41:59 +0000

    Feat: Set URL paths

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,18 @@
 from . import views
+from .views import BookingListView, BookingDetailView, BookingCreateView, BookingUpdateView, BookingDeleteView
 from django.urls import path, include
 
 
 urlpatterns = [
 
     path('', views.index, name='index'),
-
     path('accounts/', include('django.contrib.auth.urls')),
+    # booking overview
+    path('my-bookings/', BookingListView.as_view(), name='booking_list'),
+    # booking details
+    path('my-bookings/<int:pk>', BookingDetailView.as_view(), name='booking_detail'),
+    # booking create, edit, delete
+    path('my-bookings/new/', BookingCreateView.as_view(), name='booking_create'),
+    path('my-bookings/edit/<int:pk>/', BookingUpdateView.as_view(), name='booking_edit'),
+    path('my-bookings/delete/<int:pk>/', BookingDeleteView.as_view(), name='booking_delete'),
 ]
