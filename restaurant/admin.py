0c7df369cdb591394ecb986b63e57e1dc3960a18
Pernilla Strandberg <pernillastrandberg87@gmail.com>
2023-12-02 20:58:20 +0000

    Maint: Formatting code

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+
 from .models import BookingModel
 
 
 # Register Admin classes for BookingModel using decorator
 @admin.register(BookingModel)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('day', 'last_name', 'first_name', 'guests', 'time', 'created_on')
-    list_filter = ('day', 'created_on')
+    list_display = ("day", "last_name", "first_name", "guests", "time", "created_on")
+    list_filter = ("day", "created_on")
